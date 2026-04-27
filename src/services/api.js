f6c2f1bab1bc6398f21a0d8eef18f9a7af2aa365
zohaib47 <zohaibimtiaz119@gmail.com>
2026-04-27 23:01:24 +0500

    commmit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-  // baseURL: 'http://localhost:5000/api',
   baseURL: 'https://truck-dispatch-system-backend.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
